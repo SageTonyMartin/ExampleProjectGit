07ca6df3b88428fa7d335adb22cd7eb378b6c602
Tony Martin <tony.martin@sage.com>
2013-04-15 07:08:12 -0600

    Updating comments in source code

--- ExampleProject/ExampleProject/Aircraft.cs
+++ ExampleProject/ExampleProject/Aircraft.cs
@@ -25,6 +25,7 @@ namespace ExampleProject
             WarpDrive
         }
 
+        // Public properties
         public float Wingspan { get; set; }
         public int NumEngines { get; set; }
         public EngineType TypeOfEngine { get; set; }
